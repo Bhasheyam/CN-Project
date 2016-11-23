50c0f57a8d13d07616b3e93b27e26bcd1f95f007
bhasheyam <bhasheyam09@gmail.com>
2016-11-23 03:41:44 -0500

    basic structure

--- src/trafficcontroller/Traffic_app.java
+++ src/trafficcontroller/Traffic_app.java
@@ -11,66 +11,64 @@ public class Traffic_app {
 	private final int btoc=5;
 	private final int btoa=4;
 	private final int dtoa=5;
-	private final int dtoc=6;
+	private  final int dtoc=6;
+	private  static int uatoB=0;
+	private static int uctod=0;
+	private static int uatod=0;
+	private  static int uctob=0;
+	private  static int ubtoc=0;
+	private  static int ubtoa=0;
+	private  static int udtoa=0;
+	private  static int udtoc=0;
+public double calculateu(int i,int j)
+{
+	double u;
+	u=(double)i/j;
+	return u;
+}
+public double caostcalculation(int i,int j)
+{
+	double d;
+	d=(double)i/j;
+	return d;
 	
-	
-	public static void Calculate(int j)
+}
+private static String Calculatepath(String s1, String s2, int i) {
+
+	double alpha;
+	if(s1.equals("A")&&s2.equals("C"))
 	{
-		if (j==1)
-		{
-			System.out.println("Calculation completed");
-			System.out.println("");
-			System.out.println("-----------------------------------------------");
-			System.out.println("");
-		}
-		else if(j==2)
-		{
-			System.out.println("Calculation completed");
-			System.out.println("");
-			System.out.println("-----------------------------------------------");
-			System.out.println("");
-		}
-		else 
-		{
-			System.out.println("Calculation completed");
-			System.out.println("");
-			System.out.println("-----------------------------------------------");
-			System.out.println("");
-			
-		}
+		
 	}
+	return "";
+}
+	
+	
+	
+	
 	public static void main(String s[])
 	{  
 
-		while(true)
-		{
+	
 			int j = 0;
-			System.out.println("select the route to sent the packet");
-			System.out.println("1:A->C");
-			System.out.println("2:B->D");
-			System.out.println("3:C->A");
-	    System .out.println("enter the Route:");
-	    try
-		{
-	    Scanner in=new Scanner(System.in);
+			System.out.println("Follwoing are the path need to be calculated for traffic in the network");
+			System.out.println("1:A->C=4");
+			System.out.println("2:B->D=3");
+			System.out.println("3:C->A=2");
+			String s1=Traffic_app.Calculatepath("A","c",4);
+			String s2=Traffic_app.Calculatepath("B","D",3);
+			String s3=Traffic_app.Calculatepath("C","A",2);
+	  System.out.println(s1);
+	  System.out.println(s2);
+	  System.out.println(s3);
+	  
+			System.out.println("Calculation completed");
+			System.out.println("");
+			System.out.println("-----------------------------------------------");
+			System.out.println("");	
 		
-			j=in.nextInt();
-		}
-		catch(Exception e)
-		{
-			System.out.println("enter 1 or 2 or 3 integer"+ e);
-		}
-		if(j>4 || j<0)
-		{
-			System.out.println("invalid entry");
-		}
-		else
-		{
-			Traffic_app.Calculate(j);
-			
-		}
 		
-		}	
 	}
+	
 
 }
